424ae7a81592ba9ff9d45959a9e6518fe8fbf970
ztrl <ztrl@naver.com>
2022-12-01 21:04:56 +0900

    WepAppContext 수정

--- src/context/WebAppContext.java
+++ src/context/WebAppContext.java
@@ -21,24 +21,24 @@ public class WebAppContext implements ServletContextListener{
 		ServletContext application = sce.getServletContext();
 		
 		//ServletContext 객체에 데이터 저장
-//		application.setAttribute("adminService", new AdminService(application));
-//		application.setAttribute("adminListService", new AdminListService(application));
-//		application.setAttribute("QnAboardListService", new BoardListService(application));
-//		application.setAttribute("createQnAService", new CreateQnAService(application));
-//		application.setAttribute("reviewboardListService", new ReviewboardListService(application));
-//		application.setAttribute("createReviewService", new CreateReviewService(application));
-//		application.setAttribute("readReviewService", new ReadReviewService(application));
-//		application.setAttribute("cartService", new CartService(application));
-//		application.setAttribute("orderService", new OrderService(application));
-//		application.setAttribute("orderListService", new OrderListService(application));
-//		application.setAttribute("productDetailService", new ProductDetailService(application));
-//		application.setAttribute("productListService", new ProductListService(application));
-//		application.setAttribute("findPasswordService", new FindPasswordService(application));
-//		application.setAttribute("joinService", new JoinService(application));
-//		application.setAttribute("loginService", new LoginService(application));
-//		application.setAttribute("updateUserService", new UpdateUserService(application));
-//		application.setAttribute("userInfoService", new UserInfoService(application));
-//		application.setAttribute("homeService", new HomeService(application));
+		application.setAttribute("adminService", new AdminService(application));
+		application.setAttribute("adminListService", new AdminListService(application));
+		application.setAttribute("QnAboardListService", new BoardListService(application));
+		application.setAttribute("createQnAService", new CreateQnAService(application));
+		application.setAttribute("reviewboardListService", new ReviewboardListService(application));
+		application.setAttribute("createReviewService", new CreateReviewService(application));
+		application.setAttribute("readReviewService", new ReadReviewService(application));
+		application.setAttribute("cartService", new CartService(application));
+		application.setAttribute("orderService", new OrderService(application));
+		application.setAttribute("orderListService", new OrderListService(application));
+		application.setAttribute("productDetailService", new ProductDetailService(application));
+		application.setAttribute("productListService", new ProductListService(application));
+		application.setAttribute("findPasswordService", new FindPasswordService(application));
+		application.setAttribute("joinService", new JoinService(application));
+		application.setAttribute("loginService", new LoginService(application));
+		application.setAttribute("updateUserService", new UpdateUserService(application));
+		application.setAttribute("userInfoService", new UserInfoService(application));
+		application.setAttribute("homeService", new HomeService(application));
 				
 		application.setAttribute("cartDAO", new CartDAO());
 		application.setAttribute("categoryDAO", new CategoryDAO());
